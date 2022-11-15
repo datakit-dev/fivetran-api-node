97867239050a8c9bd7e3b4761df2a61ca047246e
Jordan Fowler <me@jordanfowler.co>
2022-11-15 15:29:21 -0700

    Changed Fivetran to FivetranAPI and requires explicity passing of key & secret or defaults to env vars.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import './fetch_polyfill';
 import { Client } from './client'
 
-const { FIVETRAN_API_KEY, FIVETRAN_API_SECRET } = process.env
+interface Args {
+    apiKey: string;
+    apiSecret: string;
+}
 
-const client = new Client(FIVETRAN_API_KEY, FIVETRAN_API_SECRET)
-export const Fivetran = {
-    ...client.operations,
+export const FivetranAPI = (args: Args) => {
+    let client: Client;
+    if (args && args.apiKey && args.apiSecret) {
+        client = new Client(args.apiKey, args.apiSecret);
+    } else if (process.env.FIVETRAN_API_KEY && process.env.FIVETRAN_API_SECRET) {
+        client = new Client(process.env.FIVETRAN_API_KEY, process.env.FIVETRAN_API_SECRET);
+    } else {
+        throw new Error('Fivetran API key and secret are required');
+    }
+    return client.operations;
 };
